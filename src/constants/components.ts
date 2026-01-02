b83692c61415f915c0392ebfd2f7c1a1dc45f75d
Bramahimsa <bramahimsa123@gmail.com>
2026-01-02 15:12:09 +0700

    navabr items

--- src/constants/components.ts
+++ src/constants/components.ts
@@ -2,6 +2,6 @@ import { NavbarItemsProps } from "@/types/Components";
 
 export const NAVBAR_ITEMS: NavbarItemsProps[] = [
   { title: "Home", href: "/", number: "0" },
-  { title: "About", href: "/about", number: "1" },
-  { title: "Projects", href: "/projects", number: "2" },
+  { title: "Overview", href: "/about", number: "1" },
+  { title: "My Projects", href: "/projects", number: "2" },
 ];
